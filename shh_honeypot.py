4dccc10451002504ff5eb7a66f2725228825ecf9
jadhav045 <jadhavtanmay199@gmail.com>
2025-04-17 11:11:51 +0530

    Initial Setup of HoneyPot Project

---
+++ shh_honeypot.py
@@ -0,0 +1,11 @@
+# Libraries 
+import logging
+from logging.handlers import RotatingFileHandler
+# Constants
+logging_format = logging.Formatter('%(message)s')
+
+# Loggers & Logging Files 
+funnel.logger = 
+# Emulated Shell
+# SSH Server + sockets
+# Provision SSH-based honeypot
